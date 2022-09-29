44817a149bd5ac782889a19da162e25237f40b3b
Taufik <tauframdani519@gmail.com>
2022-09-29 08:43:24 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,240 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
+  String dropdownvalue = 'Item 1';
+
+  final _formkey = GlobalKey<FormState>();
+  final TextEditingController _pass = TextEditingController();
+  final TextEditingController _confirmpass = TextEditingController();
+  final TextEditingController _username = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /* const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter a Text',
+                  ),
+                ),
+              ), */
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: _username,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.abc),
+                    border: UnderlineInputBorder(),
+                    labelText: 'Username',
+                    hintText: 'Input Username',
+                    hintStyle:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    labelStyle:
+                        TextStyle(fontSize: 15, color: Colors.redAccent),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Username';
+                    } else if (!emailRegex.hasMatch(value)) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: _pass,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.key),
+                    border: UnderlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Your Password',
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Your Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: _confirmpass,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.key),
+                    border: UnderlineInputBorder(),
+                    labelText: 'Confirm Password',
+                    hintText: 'Confirm Your Password',
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    } else if (value != _pass.text) {
+                      return 'Password Not Equal Confirm Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              /* Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: Column(children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text('Pilihan'),
+                      Container(width: 8),
+                      DropdownButton(
+                        value: dropdownvalue,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: items.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(items),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownvalue = newValue!;
+                          });
+                          print(newValue);
+                        },
+                      ),
+                    ],
+                  )
+                ]),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: DropdownButtonFormField(
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.interpreter_mode_outlined),
+                    border: UnderlineInputBorder(),
+                    labelText: 'Enter a Text',
+                  ),
+                  value: dropdownvalue,
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  items: items.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(items),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownvalue = newValue!;
+                    });
+                    print(newValue);
+                  },
+                ),
+              ), */
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
+                      /*  ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(_confirmpass.text),
+                        ),
+                      ); */
+                      showModalBottomSheet<void>(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return SizedBox(
+                              height: 200,
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: const <Widget>[
+                                    Text('Login Sucess'),
+                                    SizedBox(
+                                      height: 50,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                      print('Process Data');
+                      _formkey.currentState!.save();
+                      print('Username: ${_username.text}');
+                      print('Password: ${_confirmpass.text}');
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  final RegExp emailRegex = RegExp(
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+}
